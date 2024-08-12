1dba61287d6416f9656d61ff310713bebf4a10a9
Connie Chin <conniec96@gmail.com>
2024-08-12 21:43:27 +0000

    Colors and photo corners.

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 const dataFromObject = {
-  view: '',
-  savedRecipes: [],
+    view: '',
+    savedRecipes: [],
 };
 const previousRecipesJSON = localStorage.getItem('i-wanna-cook-local-storage');
 if (previousRecipesJSON !== null) {
-  dataFromObject.savedRecipes = JSON.parse(previousRecipesJSON);
+    dataFromObject.savedRecipes = JSON.parse(previousRecipesJSON);
 }
 function forBeforeUnload() {
-  const recipesJSON = JSON.stringify(dataFromObject.savedRecipes);
-  localStorage.setItem('i-wanna-cook-local-storage', recipesJSON);
+    const recipesJSON = JSON.stringify(dataFromObject.savedRecipes);
+    localStorage.setItem('i-wanna-cook-local-storage', recipesJSON);
 }
 window.addEventListener('beforeunload', forBeforeUnload);
